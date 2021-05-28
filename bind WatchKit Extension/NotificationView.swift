14f021a81524c0751aec6897f4f922a5aa9d9001
Wilton Ramos <wrs@cin.ufpe.br>
2021-05-28 10:39:59 -0300

    First commit

---
+++ bind WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  bind WatchKit Extension
+//
+//  Created by Wilton Ramos on 28/05/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
---
+++ bind WatchKit Extension/bindApp.swift
@@ -0,0 +1,21 @@
+//
+//  bindApp.swift
+//  bind WatchKit Extension
+//
+//  Created by Wilton Ramos on 28/05/21.
+//
+
+import SwiftUI
+
+@main
+struct bindApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
