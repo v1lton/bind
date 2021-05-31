51a670f5207d56b29f7bd70afe4523bb9a686068
Ana Melo <anacsoaresmelo@gmail.com>
2021-05-31 11:42:59 -0300

    Cria base da tela atividades

---
+++ bind WatchKit Extension/ActivityView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityView.swift
+//  bind WatchKit Extension
+//
+//  Created by Ana Carolina Soares de Melo on 31/05/21.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityView()
+    }
+}
