6134e81b4dbb0c58732d63b035a148ab325486f5
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-06-03 08:48:17 -0300

    Creates a basis to screen prototype

--- bind WatchKit Extension/InicialView.swift
+++ bind WatchKit Extension/InicialView.swift
@@ -14,49 +14,53 @@ struct InicialView: View {
     @State private var history = false
     
     var body: some View {
-        VStack (alignment: .leading, spacing: 10){
-            Text("Bind")
+        VStack (alignment: .leading){
+            
+            BarChart(title: "Monthly Sales", legend: "EUR", barColor: .blue, data: chartDataSet)
+//                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
             Spacer()
             Button(
                 action: {
                     self.modal.toggle() //true
                 },
                 label: {
-                    Text("Humor")
+                    Text("Registrar")
                 }
             )
             .sheet(isPresented: $modal, content: {
                 HumorView(modal:self.$modal)
             })
-            Button(
-                action: {
-                    self.addRecord.toggle() //true
-                },
-                label: {
-                    Text("Registrar")
-                }
-            )
-            .sheet(isPresented: $addRecord, content: {
-                TemporaryAddHistory(modal: self.$addRecord)
-            })
             
-            Button(
-                action: {
-                    self.history.toggle() //true
-                },
-                label: {
-                    Text("Histórico")
-                }
-            )
-            .sheet(isPresented: $history, content: {
-                HistoryView(modal: self.$history)
-                    .toolbar(content: {
-                        ToolbarItem(placement: .cancellationAction) {
-                            Button("Close") { self.history.toggle()
-                            }
-                        }
-                    })
-            })
+//            Button(
+//                action: {
+//                    self.addRecord.toggle() //true
+//                },
+//                label: {
+//                    Text("Registrar")
+//                }
+//            )
+//            .sheet(isPresented: $addRecord, content: {
+//                TemporaryAddHistory(modal: self.$addRecord)
+//            })
+            
+//            Button(
+//                action: {
+//                    self.history.toggle() //true
+//                },
+//                label: {
+//                    Text("Histórico")
+//                }
+//            )
+//            .sheet(isPresented: $history, content: {
+//                HistoryView(modal: self.$history)
+//                    .toolbar(content: {
+//                        ToolbarItem(placement: .cancellationAction) {
+//                            Button("Close") { self.history.toggle()
+//                            }
+//                        }
+//                    })
+//            })
         }
     }
 }
